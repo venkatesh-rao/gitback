9e609eb087ee4f04d41e64e69f4b4790e5e23de3
Venkatesh Rao <venkatvr897@gmail.com>
2020-05-04 17:23:58 +0530

    web-app: configure react-router and apollo-client

--- packages/web-app/src/App.tsx
+++ packages/web-app/src/App.tsx
@@ -1,8 +1,25 @@
+import { ApolloProvider } from "@apollo/client";
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import client from "./apollo/apollo-config";
+import Home from "./views/Home/index";
 import Login from "./views/Login";
 
 function App() {
-  return <Login />;
+  return (
+    <ApolloProvider client={client}>
+      <Router>
+        <Switch>
+          <Route exact path="/login">
+            <Login />
+          </Route>
+          <Route exact path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+    </ApolloProvider>
+  );
 }
 
 export default App;
---
+++ packages/web-app/src/apollo/apollo-config.ts
@@ -0,0 +1,11 @@
+import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri: process.env.REACT_APP_API_ENDPOINT,
+    // credentials: "include",
+  }),
+});
+
+export default client;
