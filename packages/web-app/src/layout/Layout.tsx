63fca6b27edc41a73711699ca147743118164483
Venkatesh Rao <venkatvr897@gmail.com>
2020-05-11 19:57:16 +0530

    web-app: integrate logout

--- packages/web-app/src/layout/Layout.tsx
+++ packages/web-app/src/layout/Layout.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 import { FaPlus } from "react-icons/fa";
 import { LoggedInUserProps } from "../components/EnhancedRoutes/ProtectedRoute";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
+import ReactDOM from "react-dom";
+import { useMutation } from "@apollo/client";
+import { LogoutData } from "./types";
+import { LOGOUT_MUTATION } from "./query";
+import { History } from "history";
 
 const { REACT_APP_GITHUB_APP_NAME } = process.env;
 
@@ -9,6 +14,8 @@ interface ILayoutProps {}
 
 const Layout: React.FC<ILayoutProps & LoggedInUserProps> = (props) => {
   const { avatarUrl, installationId } = props.loggedInUser!;
+
+  const history = useHistory();
 
   const buttonProps = {
     className:
@@ -26,9 +33,12 @@ const Layout: React.FC<ILayoutProps & LoggedInUserProps> = (props) => {
     <div>
       <nav className="flex items-center justify-between flex-wrap bg-white p-3 shadow-md">
         <div className="flex items-center flex-grow mr-6">
-          <span className="text-purple-700 font-semibold text-xl tracking-wide">
+          <Link
+            to="/"
+            className="text-purple-700 font-semibold text-xl tracking-wide"
+          >
             GITBACK
-          </span>
+          </Link>
         </div>
         <div className="flex items-center">
           {installationId ? (
@@ -43,12 +53,104 @@ const Layout: React.FC<ILayoutProps & LoggedInUserProps> = (props) => {
               {buttonChildren}
             </a>
           )}
-          <div className="ml-3">
-            <img className="w-6 h-6 shadow-xs rounded-full" src={avatarUrl} />
-          </div>
+          <Avatar avatarUrl={avatarUrl!} history={history} />
         </div>
       </nav>
       <main className="p-6">{props.children}</main>
+    </div>
+  );
+};
+
+/**
+ * Hook that alerts clicks outside of the passed ref
+ */
+function useOutsideAlerter(
+  ref: React.RefObject<HTMLDivElement>,
+  callback: () => void
+) {
+  React.useEffect(() => {
+    /**
+     * Alert if clicked on outside of element
+     */
+    function handleClickOutside(this: Document, ev: MouseEvent) {
+      if (
+        ev.target instanceof HTMLElement &&
+        !ReactDOM.findDOMNode(ref.current)!.contains(ev.target)
+      )
+        if (ref.current && !ref.current.contains(ev.target)) {
+          callback();
+        }
+    }
+
+    // Bind the event listener
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      // Unbind the event listener on clean up
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [ref]);
+}
+
+interface IAvatarProps {
+  avatarUrl: string;
+  history: History<History.PoorMansUnknown>;
+}
+
+const Avatar: React.FC<IAvatarProps> = (props: IAvatarProps) => {
+  const { avatarUrl, history } = props;
+  const [menuOpen, setMenuOpen] = React.useState(false);
+
+  const menuOpenRef = React.useRef(menuOpen);
+
+  const wrapperRef = React.useRef<HTMLDivElement>(null);
+  useOutsideAlerter(wrapperRef, () => {
+    if (menuOpenRef.current) {
+      setMenuOpen(false);
+      menuOpenRef.current = false;
+    }
+  });
+
+  const toggleMenu = React.useCallback(() => {
+    setMenuOpen((prev) => !prev);
+    menuOpenRef.current = !menuOpenRef.current;
+  }, []);
+
+  const [logout, { loading: logoutLoading }] = useMutation<LogoutData>(
+    LOGOUT_MUTATION
+  );
+
+  const handleLogout = React.useCallback(async () => {
+    if (logoutLoading) return;
+    try {
+      await logout();
+    } catch (err) {}
+    toggleMenu();
+    history.push("/");
+  }, [logout, logoutLoading]);
+
+  return (
+    <div className="ml-3 relative" ref={wrapperRef}>
+      <img
+        role="button"
+        className="w-6 h-6 shadow-xs rounded-full"
+        src={avatarUrl}
+        onClick={toggleMenu}
+      />
+      <div
+        className={`${
+          menuOpen ? "block" : "hidden"
+        } absolute right-0 py-2 w-40 rounded shadow-md bg-white`}
+      >
+        <ul>
+          <li
+            role="button"
+            onClick={handleLogout}
+            className="text-red-500 py-1 px-2 text-center cursor-pointer bg-white hover:bg-red-100"
+          >
+            Logout
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
---
+++ packages/web-app/src/layout/types.ts
@@ -0,0 +1,3 @@
+export interface LogoutData {
+  logout: boolean;
+}
