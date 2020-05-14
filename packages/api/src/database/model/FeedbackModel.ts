7e4d7301064ef9c02a24b7309d81971e29230601
Venkatesh Rao <venkatvr897@gmail.com>
2020-05-14 20:54:15 +0530

    api: remove unwanted fields on feedback model

--- packages/api/src/database/model/FeedbackModel.ts
+++ packages/api/src/database/model/FeedbackModel.ts
@@ -6,26 +6,10 @@ import { IStatus } from "./StatusModel";
 // Schema
 const FeedbackSchema = new Schema(
   {
-    title: {
-      type: String,
-      required: true,
-    },
-    description: {
-      type: String,
-    },
-    status: {
-      type: Schema.Types.ObjectId,
-      ref: "Status",
-      required: true,
-    },
     product: {
       type: Schema.Types.ObjectId,
       ref: "Product",
       required: true,
-    },
-    owner: {
-      type: Schema.Types.ObjectId,
-      ref: "User",
     },
     issueNumber: {
       type: Number,
@@ -39,8 +23,6 @@ const FeedbackSchema = new Schema(
 
 // DO NOT export this
 interface IFeedbackSchema extends Document {
-  title: string;
-  description?: string;
   issueNumber: number;
 }
 
@@ -50,8 +32,6 @@ interface IFeedbackBase extends IFeedbackSchema {}
 // Export this for strong typing
 export interface IFeedback extends IFeedbackBase {
   product: IProduct["_id"];
-  owner: IUser["_id"];
-  status: IStatus["_id"];
 }
 
 // Export this for strong typing
@@ -59,32 +39,14 @@ export interface IFeedback_product_populated extends IFeedbackBase {
   product: IProduct;
 }
 
-export interface IFeedback_owner_populated extends IFeedbackBase {
-  owner: IUser;
-}
-
-export interface IFeedback_status_populated extends IFeedbackBase {
-  status: IStatus;
-}
-
 // Static methods
-FeedbackSchema.statics.findMyOwner = async function (id: string) {
-  return this.findById(id).populate("owner").exec();
-};
-
 FeedbackSchema.statics.findMyProduct = async function (id: string) {
   return this.findById(id).populate("product").exec();
 };
 
-FeedbackSchema.statics.findMyStatus = async function (id: string) {
-  return this.findById(id).populate("status").exec();
-};
-
 // For model
 export interface IFeedbackModel extends Model<IFeedback> {
-  findMyOwner(id: string): Promise<IFeedback_owner_populated>;
   findMyProduct(id: string): Promise<IFeedback_product_populated>;
-  findMyStatus(id: string): Promise<IFeedback_status_populated>;
 }
 
 // Default export
