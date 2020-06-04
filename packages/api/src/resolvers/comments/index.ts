6777be3544c04bdf819c0f429dcc7c2dd24837d3
Venkatesh Rao <venkatvr897@gmail.com>
2020-06-04 11:43:46 +0530

    api: add code for creating a comment on feedback

--- packages/api/src/resolvers/comments/index.ts
+++ packages/api/src/resolvers/comments/index.ts
@@ -1,9 +1,15 @@
 import { ContextWithDBModel } from "../../types";
-import { QueryCommentsArgs } from "../../generated/graphql";
+import {
+  QueryCommentsArgs,
+  MutationCreateCommentArgs,
+} from "../../generated/graphql";
 import { request } from "@octokit/request";
 import { app } from "../../utils/github";
 
-export async function getComments(args: any, context: ContextWithDBModel) {
+export async function getComments(
+  args: QueryCommentsArgs,
+  context: ContextWithDBModel
+) {
   const { productUrl, issueNumber, limit = 10, offset } = args;
 
   const product = await context.db.Product.findOne({ url: productUrl })
@@ -11,7 +17,7 @@ export async function getComments(args: any, context: ContextWithDBModel) {
     .lean();
 
   if (!product) {
-    throw new Error("invalid product");
+    throw new Error("Product not found!");
   }
 
   const { owner, repositoryName } = product;
@@ -28,7 +34,7 @@ export async function getComments(args: any, context: ContextWithDBModel) {
       owner: repoOwner,
       repo: repoName,
       issue_number: issueNumber,
-      per_page: limit,
+      per_page: limit!,
       page: offset,
       headers: {
         authorization: `token ${appInstallationAccessToken}`,
@@ -45,4 +51,59 @@ export async function getComments(args: any, context: ContextWithDBModel) {
     createdAt: new Date(comment.created_at).getTime(),
     updatedAt: new Date(comment.updated_at).getTime(),
   }));
+}
+
+export async function addNewComment(
+  args: MutationCreateCommentArgs,
+  context: ContextWithDBModel
+) {
+  const { productUrl, issueNumber, commentBody } = args;
+
+  const { githubUserAccessToken } = context.req;
+
+  const product = await context.db.Product.findOne({ url: productUrl })
+    .populate("owner")
+    .lean();
+
+  if (!product) {
+    throw new Error("Product not found!");
+  }
+
+  const { owner, repositoryName } = product;
+
+  let accessToken = githubUserAccessToken;
+
+  if (!accessToken) {
+    accessToken = await app.getInstallationAccessToken({
+      installationId: owner.installationId,
+    });
+  }
+
+  const [repoOwner, repoName] = repositoryName.split("/");
+
+  const commentResponse = await request(
+    "POST /repos/:owner/:repo/issues/:issue_number/comments",
+    {
+      owner: repoOwner,
+      repo: repoName,
+      issue_number: issueNumber,
+      body: commentBody,
+      headers: {
+        authorization: `token ${accessToken}`,
+      },
+    }
+  );
+
+  const { data: comment } = commentResponse;
+
+  return {
+    id: comment.id,
+    body: comment.body,
+    user: {
+      username: comment.user.login,
+      avatarUrl: comment.user.avatar_url,
+    },
+    createdAt: new Date(comment.created_at).getTime(),
+    updatedAt: new Date(comment.updated_at).getTime(),
+  };
 }
